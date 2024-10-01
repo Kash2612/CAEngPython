11a1221be7a5d4a1c97149b82ca2e05244f5630c
kash2612 <kashgupta20@gmail.com>
2024-10-02 00:31:14 +0530

    added docker file

--- student_management/settings.py
+++ student_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e^gt@)^e0b9m)1fx-fu+0t3xb$@5x@b)di!ze5@rsdvoo01$+8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
